f530bcccc82ce234c2083bba7f245ec0f64a9e68
Maksym Cherniavskyi <cherniavskyi.maksym@lll.kpi.ua>
2025-06-05 12:26:04 +0300

    Update task1.c

--- task1.c
+++ task1.c
@@ -3,11 +3,11 @@
 #include <stdint.h>
 
 int main() {
-    printf("Розмір time_t: %zu байт\n", sizeof(time_t));
+    printf("Size of time_t: %zu bytes\n", sizeof(time_t));
+
     time_t max = (time_t)(~((time_t)1 << (sizeof(time_t) * 8 - 1)));
+    printf("Maximum time_t value: %lld\n", (long long)max);
 
-    printf("Максимальне значення time_t: %lld\n", (long long)max);
-    printf("Дата переповнення: %s", ctime(&max));
-
+    printf("Overflow date: %s", ctime(&max));
     return 0;
 }
